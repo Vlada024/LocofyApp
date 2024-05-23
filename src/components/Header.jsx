dd1b0f30ed2ccfdb9cba8371c8c4d749f93e977d
Vlada024 <Vlada024>
2024-05-23 22:39:45 +0000

    Pushed from Locofy Builder

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import styles from "./Header.module.css";
+
+const Header = ({ logo, notifications, avatar, showLoginSection }) => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerContent}>
+        <img className={styles.logoIcon} loading="lazy" alt="" src={logo} />
+        <div className={styles.navLinks}>
+          <div className={styles.home}>Home</div>
+          <div className={styles.stays}>Stays</div>
+          <div className={styles.becomeAHost}>Become a host</div>
+        </div>
+        {showLoginSection && (
+          <div className={styles.loginSection}>
+            <img
+              className={styles.notificationsIcon}
+              loading="lazy"
+              alt=""
+              src={notifications}
+            />
+            <img
+              className={styles.avatarIcon}
+              loading="lazy"
+              alt=""
+              src={avatar}
+            />
+          </div>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,67 @@
+import { useCallback } from "react";
+import { Button } from "@mui/material";
+import Header from "../components/Header";
+import HeroContainer from "../components/HeroContainer";
+import SearchSectionHeader from "../components/SearchSectionHeader";
+import CardList from "../components/CardList";
+import Footer from "../components/Footer";
+import styles from "./Homepage.module.css";
+
+const Homepage = () => {
+  const onShowMoreButtonClick = useCallback(() => {
+    //TODO: Please show more items on click
+  }, []);
+
+  return (
+    <div className={styles.homepage}>
+      <Header
+        logo="/logo.svg"
+        notifications="/notifications@2x.png"
+        avatar="/avatar@2x.png"
+        showLoginSection
+      />
+      <HeroContainer />
+      <main className={styles.staysSection}>
+        <SearchSectionHeader />
+        <CardList />
+        <Button
+          className={styles.showMoreButton}
+          disableElevation={true}
+          variant="outlined"
+          sx={{
+            textTransform: "none",
+            color: "#00c29f",
+            fontSize: "16",
+            borderColor: "#00c29f",
+            borderRadius: "6px",
+            "&:hover": { borderColor: "#00c29f" },
+            width: 147,
+            height: 48,
+          }}
+          onClick={onShowMoreButtonClick}
+        >
+          Show more
+        </Button>
+      </main>
+      <Footer />
+      <div className={styles.band}>
+        <div className={styles.bandInner}>
+          <div className={styles.localhostIncAllRightsResParent}>
+            <div className={styles.localhostIncAll}>
+              © 2023 Localhost, Inc. All Rights Reserved
+            </div>
+            <div className={styles.policyNavigation}>
+              <div className={styles.privacyPolicy}>Privacy Policy</div>
+              <div
+                className={styles.termsConditions}
+              >{`Terms & Conditions`}</div>
+              <div className={styles.contactUs}>Contact us</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
